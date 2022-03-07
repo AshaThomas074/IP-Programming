95fbe302e55e013600ee8f69c8e847277b4b746c
AshaThomas074 <ashathomas74@gmail.com>
2022-03-07 17:03:22 +0000

    moving button to left when any key pressed

--- week5/exercise4.js
+++ week5/exercise4.js
@@ -1,21 +1,18 @@
-function myFunction(){
-    var element=document.getElementById("circle");
-    element.style.opacity='1';    
-    element.addEventListener('click',myNewFunction);
+function myFunction(){ 
+    document.addEventListener('keydown',myNewFunction);
 }
 
 function myNewFunction()
 {
-    setInterval(myTimerAction,1000);
-    
+    var element=document.getElementById("circle");    
+    var positionLeft=element.offsetLeft;
+    element.style.left=positionLeft-10+'px';    
 }
 
 function myTimerAction(){
     var element=document.getElementById("circle");    
     var positionLeft=element.offsetLeft;
     element.style.left=positionLeft-10+'px';
-    //var circleOpacity=parseFloat(element.style.opacity);
-    //element.style.opacity=circleOpacity-0.1;  
 }
 
 document.addEventListener('DOMContentLoaded',myFunction);
