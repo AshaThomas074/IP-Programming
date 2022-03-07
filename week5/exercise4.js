933f645bd152541bf0f55e45186829243fdaac32
AshaThomas074 <ashathomas74@gmail.com>
2022-03-07 17:10:48 +0000

    logging keycode to console

--- week5/exercise4.js
+++ week5/exercise4.js
@@ -1,5 +1,5 @@
 function myFunction(){ 
-    document.addEventListener('keydown',myNewFunction);
+    document.addEventListener('keydown',myKeyDown);
 }
 
 function myNewFunction()
@@ -9,10 +9,8 @@ function myNewFunction()
     element.style.left=positionLeft-10+'px';    
 }
 
-function myTimerAction(){
-    var element=document.getElementById("circle");    
-    var positionLeft=element.offsetLeft;
-    element.style.left=positionLeft-10+'px';
+function myKeyDown(event){
+    console.log(event.keyCode);
 }
 
 document.addEventListener('DOMContentLoaded',myFunction);
