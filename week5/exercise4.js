cc0112ad3e0fc5767c7b86485e6751de61f3ed57
AshaThomas074 <ashathomas74@gmail.com>
2022-03-07 17:32:55 +0000

    moving button to top,left,right,down using relevant keys

--- week5/exercise4.js
+++ week5/exercise4.js
@@ -2,15 +2,46 @@ function myFunction(){
     document.addEventListener('keydown',myKeyDown);
 }
 
-function myNewFunction()
+function moveLeft()
 {
     var element=document.getElementById("circle");    
     var positionLeft=element.offsetLeft;
     element.style.left=positionLeft-10+'px';    
 }
+function moveUp()
+{
+    var element=document.getElementById("circle");    
+    var positionTop=element.offsetTop;
+    element.style.top=positionTop-10+'px';    
+}
+function moveRight()
+{
+    var element=document.getElementById("circle");    
+    var positionLeft=element.offsetLeft;
+    element.style.left=positionLeft+10+'px';    
+}
+function moveDown()
+{
+    var element=document.getElementById("circle");    
+    var positionTop=element.offsetTop;
+    element.style.top=positionTop+10+'px';    
+}
 
 function myKeyDown(event){
     console.log(event.keyCode);
+    if(event.keyCode == 37){
+        moveLeft();
+    }
+    if(event.keyCode == 38){
+        moveUp();
+    }
+    if(event.keyCode == 39){
+        moveRight();
+    }
+    if(event.keyCode == 40){
+        moveDown();
+    }
+
 }
 
 document.addEventListener('DOMContentLoaded',myFunction);
